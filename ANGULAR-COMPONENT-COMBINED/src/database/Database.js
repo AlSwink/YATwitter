0397335afd869b4ebb023669719619cfc28a4515
Eli Rarey <erarey@gmail.com>
2017-04-04 13:15:07 -0500

    Updated connections between components
    lists on sides tested working, though no database connection yet

--- ANGULAR-COMPONENT-COMBINED/src/database/Database.js
+++ ANGULAR-COMPONENT-COMBINED/src/database/Database.js
@@ -7,14 +7,38 @@ angular.module('twitter.app')
   console.log('Database service created')
   console.log(this)
   $http({
-    method:'GET',
-    url: '' //I had trouble accessing Google, Google rejected my request. May need to add headers: key
-
-  }).then(function successCallback(response){
+    method: 'GET',
+    url: '' // I had trouble accessing Google, Google rejected my request. May need to add headers: key
+  }).then(function successCallback (response) {
     console.log('was able to access index.html')
-  }, function errorCallback(response){
+  }, function errorCallback (response) {
     console.log('http test failed, could not access index.html')
   })
 
+  this.get = function (type) {
+    $http({
+      method: 'GET',
+      url: '' // I had trouble accessing Google, Google rejected my request. May need to add headers: key
+      //same origin requests?
+    }).then(function successCallback (response) {
+      console.log('SUCCESSFULLY CALLED METHOD')
+    }, function errorCallback (response) {
+      console.log('FAILED TO GET RETURN FROM METHOD')
+    })
+  }
+
+  this.getAll = function (type) {
+      //$http method here
+      console.log ("called database getAll with " + type + " as type")
+      if (type === 'people') {
+        return ['Bob', 'Jerry', 'Adam', 'Samantha', 'Keesha', 'Amanda', 'Erv']
+      }
+      else if (type === 'hashtags') {
+        return ['PortlandVirtues', 'leftofcenter', 'crazytown', 'somenonsense', 'whereisthechicken']
+      }
+      else {
+        return []
+      }
+  }
 
 })
--- ANGULAR-COMPONENT-COMBINED/src/features/searchboxwithlist/component.js
+++ ANGULAR-COMPONENT-COMBINED/src/features/searchboxwithlist/component.js
@@ -7,12 +7,37 @@ export default angular.module('twitter.app')
   templateUrl, //comes from the import, installs into the templateUrl slot as if templateUrl: templateUrl
 
   controller: function(Database /*services controller needs access to*/){
-    console.log(this)
+
+    this.getUser = () => {
+      console.log("executing GET")
+      Database.getUser();
+    }
+
+    this.setType = (type) => {
+      this.type = type;
+    }
+
+    this.refresh = () => {
+      this.listdata = Database.getAll(this.type)
+    }
+
+    //this.type = ''  this must be commented out b/c type arrives via the bindings
+    //as a parameter from index.html. Uncommented, it is hidden or overwritten to ''
+
+    this.item = 'some item'
+
+    this.listdata = []
+
+    this.refresh()
+    //this.$onChanges = this.refresh()
     //this.somefunction = function(){}
     //goes here. functions that will be called by html through the bindings.
   },
   controllerAs: 'ctrl',
-  bindings: {}
+  bindings: {
+    type: '@',
+    item: '@'
+  }
 })
 
 //because there is no array on the module here, angular will send us the module named
