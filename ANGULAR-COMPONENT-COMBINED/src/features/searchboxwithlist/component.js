6a88720af6902738e0978cbf6ca9df7fbd790364
Eli Rarey <erarey@gmail.com>
2017-04-05 18:33:16 -0500

    Continuing to update side lists
    implementing toolbar in progress on side menu, prepping to merge changes to main.css and index.html

--- ANGULAR-COMPONENT-COMBINED/src/features/searchboxwithlist/component.js
+++ ANGULAR-COMPONENT-COMBINED/src/features/searchboxwithlist/component.js
@@ -2,15 +2,17 @@ import templateUrl from './template.html'
 
 console.log("loaded searchboxwithlist")
 
+let self001
 export default angular.module('twitter.app')
 .component('searchboxwithlist', {
   templateUrl, //comes from the import, installs into the templateUrl slot as if templateUrl: templateUrl
 
-  controller: function(Database /*services controller needs access to*/){
+  controller: function(Database, $timeout, $q, $log) {
+    self001 = this
 
-    this.getUser = () => {
-      console.log("executing GET")
-      Database.getUser();
+    this.get = (type) => {
+      console.log("trying to GET with type " + type)
+      //Database.get(type);
     }
 
     this.setType = (type) => {
@@ -18,16 +20,63 @@ export default angular.module('twitter.app')
     }
 
     this.refresh = () => {
-      this.listdata = Database.getAll(this.type)
+      this.listdata = ['dummy1', 'dummy2', 'dummy3', 'whatever', 'Alpha', 'Bravo', 'Delta', 'Echo', 'Foxtrot', '1', 'a', 'b']
+       //Database.getAll(this.type)
     }
 
     //this.type = ''  this must be commented out b/c type arrives via the bindings
     //as a parameter from index.html. Uncommented, it is hidden or overwritten to ''
-
     this.item = 'some item'
 
     this.listdata = []
 
+    this.searchEnabled = true
+
+    this.sortByTrending = true
+
+    this.sortByPeople = true
+
+    this.sortByTags = true
+
+    this.peopleSearchVisible = true
+
+    this.tagSearchVisible = false
+
+    this.menuState = 'people'
+
+    this.isMenuState = (m) => m === this.menuState
+
+    this.getMenuState = () => this.menuState
+
+    this.getSearchEnabled = () => this.searchEnabled
+    //complicated so that it is easy to add more states of visibility
+    this.setMenuState = (m) => this.menuState = m
+
+    this.toggleTagSearch = () => this.tagSearchVisible = !this.tagSearchVisible
+
+    this.toggleSortByPeople = () => this.sortByPeople = !this.sortByPeople
+
+    this.toggleSearchEnabled = () => {this.searchEnabled = !(this.getSearchEnabled()); console.log(this.searchEnabled.toString());}
+
+    this.toggleByTrending = () => this.sortByTrending = !this.sortByTrending
+
+    this.toggleVisible = () => this.toggleVisible = !this.toggleVisible
+
+
+
+    this.new = function(item){
+      if (type === 'hashtags')
+      {
+        console.log('IMPLEMENT CREATE NEW HASHTAG FROM SEARCH BAR')
+      }
+      else if (type === 'people')
+      {
+        console.log('IMPLEMENT CREATE NEW HASHTAG FROM SEARCH BAR')
+      }
+      else {
+        console.log('searchbox type error! Received: ' + type)
+      }
+    }
     this.refresh()
     //this.$onChanges = this.refresh()
     //this.somefunction = function(){}
